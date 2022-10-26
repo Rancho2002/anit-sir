9b115ea609aaed26e44a3373a3757ae45d989ef1
Arijit <subhasghosh196@outlook.com>
2022-10-26 21:37:23 +0530

    added q2

--- assignment2.py
+++ assignment2.py
@@ -5,4 +5,4 @@ def wordOccur(word):
     return my_text.count(word)
 print("The number of sentence present in .docx file is:",sentence)
 target=input("Which word you want to find? ")
-print(f"The number {target} is present in the .docx file is:",wordOccur(target))
+print(f"The word {target}, is present in the .docx file is:",wordOccur(target))
