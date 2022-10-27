d5fe74553545174e45f6055c3f9537cc5fe0142a
Arijit <subhasghosh196@outlook.com>
2022-10-27 15:12:50 +0530

    modified the errors

--- assignment1.py
+++ assignment1.py
@@ -13,6 +13,17 @@ class Employee(Human):
         self.location=location
         self.role=role
         self.salary=salary
+    def showDetails(self):
+        print("Name: "+ self.name)
+        print("Gender: "+ self.gender)
+        print("Age: "+ str(self.age))
+        print("Blood Group: "+ self.blood)
+        print("Marital Status: "+ self.marital)
+        print("Company Name: "+ self.company)
+        print("Employee ID: "+ self.id)
+        print("Location: "+ self.location)
+        print("Role: "+ self.role)
+        print("Salary: "+ self.salary)
 
 class Private(Employee):
 
@@ -29,16 +40,7 @@ class Private(Employee):
         self.swags= self.swags-1
     
     def showDetails(self):
-        print("Name: "+ self.name)
-        print("Gender: "+ self.gender)
-        print("Age: "+ str(self.age))
-        print("Blood Group: "+ self.blood)
-        print("Marital Status: "+ self.marital)
-        print("Company Name: "+ self.company)
-        print("Employee ID: "+ self.id)
-        print("Location: "+ self.location)
-        print("Role: "+ self.role)
-        print("Salary: "+ self.salary)
+        super().showDetails()
         print("Bonuses Recieved: "+ str(self.bonus))
         print("Swags Left: "+ str(self.swags))
 
@@ -58,17 +60,27 @@ class Government(Employee):
         self.trips=self.trips-1
     
     def showDetails(self):
-        print("Name: "+ self.name)
-        print("Gender: "+ self.gender)
-        print("Age: "+ str(self.age))
-        print("Blood Group: "+ self.blood)
-        print("Marital Status: "+ self.marital)
-        print("Company Name: "+ self.company)
-        print("Employee ID: "+ self.id)
-        print("Location: "+ self.location)
-        print("Role: "+ self.role)
-        print("Salary: "+ self.salary)
+        super().showDetails()
         print("Holidays Left: "+ str(self.holiday))
         print("Trips Left: "+ str(self.trips))
 
 
+a1= Private()
+a1.fillDetails("Arijit Ghosh","M",20, "B+","Unmarried")
+a1.member("Google","G-2939","UK","SDE","100K")
+a1.showDetails()
+a1.claimBonus(100)
+a1.claimSwag()
+a1.claimSwag()
+print("\n")
+a1.showDetails()
+print("\n")
+a2=Government()
+a2.fillDetails("Devika Ghosh","F",20,"B+","Unmarried")
+a2.member("Canary Bank","C-48392","Kolkata","Officer","60K")
+print("\n")
+a2.showDetails()
+a2.claimHoliday()
+a2.claimTrip()
+print("\n")
+a2.showDetails()
--- assignment2.py
+++ assignment2.py
@@ -1,8 +1,15 @@
 import docx2txt
 my_text = docx2txt.process("demo.docx")
+arr=my_text.split(" ")
+eachArr=list(set(arr))
 sentence=len(my_text.split("."))
+
 def wordOccur(word):
     return my_text.count(word)
-print("The number of sentence present in .docx file is:",sentence)
-target=input("Which word you want to find? ")
-print(f"The word {target}, is present in the .docx file is:",wordOccur(target))
+
+print("The number of sentence present in .docx file is:",sentence,"\n")
+
+
+print(eachArr)
+for i in range(len(eachArr)):
+    print(f"The word '{eachArr[i]}' is present in the .docx file is:",wordOccur(eachArr[i]))
