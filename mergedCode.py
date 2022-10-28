2ba66c578474408513e58dba00c31ffc236eb937
Arijit <subhasghosh196@outlook.com>
2022-10-28 21:02:36 +0530

    Assignment sol

---
+++ mergedCode.py
@@ -0,0 +1,15 @@
+# The merged Link : https://github.com/metafy-social/python-scripts/pull/274
+
+import random
+def otp(num):
+    result=""
+    if(len(str(num))==10):
+        for i in range(6):
+            digit=str(random.randint(0,9))
+            result=result+digit
+        return "Your OTP is: "+result
+    else:
+        s="Enter a valid mobile number!"
+        return s
+number=int(input("Enter mobile number: "))
+print(otp(number))
--- readme.md
+++ readme.md
@@ -4,3 +4,11 @@
 ```
 pip install docx2txt
 ```
+
+# Assignment 4: 
+
+### Merged Link
+
+```
+https://github.com/metafy-social/python-scripts/pull/274
+```
